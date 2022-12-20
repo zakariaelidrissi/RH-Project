9841784f581c975168cdf0a9dbe988d5dd92fa4b
KHTOU Otmane <khtouotman@gmail.com>
2022-12-20 09:46:57 +0100

    stagiaire add

---
+++ stagiaire/src/main/java/com/rh/stagiaire/Model/User.java
@@ -0,0 +1,20 @@
+package com.rh.stagiaire.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class User {
+    private Long id;
+    private String genre;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String motDePasse;
+    private String tel;
+    private String userRole;
+    private boolean emailVerified;
+    private String verificationCode;
+}
---
+++ stagiaire/src/test/java/com/rh/stagiaire/StagiaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.stagiaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagiaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
