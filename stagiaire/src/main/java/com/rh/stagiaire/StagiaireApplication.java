60f73c1c9c7593596378c371274fe591ea2191d5
KHTOU Otmane <khtouotman@gmail.com>
2022-12-27 10:38:03 +0100

    azertyui

--- stagiaire/src/main/java/com/rh/stagiaire/StagiaireApplication.java
+++ stagiaire/src/main/java/com/rh/stagiaire/StagiaireApplication.java
@@ -5,12 +5,17 @@ import com.rh.stagiaire.Repositories.StagiaireRepository;
 import com.rh.stagiaire.Service.StagiaireService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import java.time.Instant;
 import java.util.Date;
 
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
+@EnableFeignClients
 @SpringBootApplication
 public class StagiaireApplication {
 
