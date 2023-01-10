ac06dda5e89544f1b1007c8665d3aa73f4589a58
KHTOU Otmane <khtouotman@gmail.com>
2023-01-10 16:21:41 +0100

    Updates 10/01

--- stagiaire/src/main/java/com/rh/stagiaire/Service/StagiaireService.java
+++ stagiaire/src/main/java/com/rh/stagiaire/Service/StagiaireService.java
@@ -1,15 +1,22 @@
 package com.rh.stagiaire.Service;
 
 import com.rh.stagiaire.Entities.Stagiaire;
+import com.rh.stagiaire.Feign.PostulationClient;
 import com.rh.stagiaire.Feign.UserClient;
+import com.rh.stagiaire.Model.OffreStage;
 import com.rh.stagiaire.Model.StagiareRequest;
 import com.rh.stagiaire.Model.User;
 import com.rh.stagiaire.Repositories.StagiaireRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -20,10 +27,15 @@ public class StagiaireService {
 
     private StagiaireRepository stagiaireRepository;
     private UserClient userRestClient;
+    private PostulationClient postulationClient;
+    JavaMailSender javaMailSender;
 
     // ************************ GET **************************
     public User getUserById(Long id) {
         return userRestClient.getUserById(id);
+    }
+    public OffreStage getOffreById(Long id) {
+        return postulationClient.getOffreById(id);
     }
 
     public List<Stagiaire> getAllStagiaire() {
@@ -78,5 +90,21 @@ public class StagiaireService {
     // ************************ DELETE **************************
     public void deleteById(Long id) {
         stagiaireRepository.deleteById(id);
+    }
+
+    // **************** EMAIL **************************
+    public void sendStartDateEmail(Long UserId, Long OffreId) {
+        try {
+            OffreStage offreStage = postulationClient.getOffreById(OffreId);
+            User user = userRestClient.getUserById(UserId);
+            MimeMessage msg = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+            helper.setTo(user.getEmail());
+            helper.setSubject("Confirmation de votre stage");
+            helper.setText("Nous sommes heureux de vous informer que votre stage commencera le " + offreStage.getDate_debut_stage() + ".");
+            javaMailSender.send(msg);
+        } catch (MessagingException e) {
+            // Traitement des erreurs
+        }
     }
 }
