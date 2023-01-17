1d703665ed02e4c1496a86c72ae31796c028d2fd
Zakaria EL Iidrissi <61559275+zakariaelidrissi@users.noreply.github.com>
2023-01-17 12:08:41 +0100

    rrrrr

--- user/src/main/java/com/RHmanagment/user/UserApplication.java
+++ user/src/main/java/com/RHmanagment/user/UserApplication.java
@@ -23,13 +23,14 @@ public class UserApplication {
     }
 	//@Bean
     CommandLineRunner start(UserService userRepository, RepositoryRestConfiguration restconfiguration){
-		return args -> {
-			restconfiguration.exposeIdsFor(User.class);
+        restconfiguration.exposeIdsFor(User.class);
+        return args -> {
 
-            userRepository.addUser(new User(null,"UA120929","Homme","KHTOU","Otmane", new Date("20/12/2000"),"khtouotman@gmail.com","123","0618566121", UserRole.ADMIN,false));
-            userRepository.addUser(new User(null,"AB984256","Homme","EL IDRISSI","Zakaria", new Date("21/12/2000"),"idrissizakaria@gmail.com","123","0612345678", UserRole.EMPLOYER,false));
-            userRepository.addUser(new User(null,"S452893","Homme","AKAJDAOU","Nabil", new Date("22/12/2000"),"akajdaounabil@gmail.com","123","0609876543",UserRole.STAGIAIRE,false));
-            userRepository.addUser(new User(null,"S452893","Femme","Maali","Abir", new Date("22/12/2000"),"akajdaounabil@gmail.com","123","0609876543",UserRole.STAGIAIRE,false));
+            userRepository.addUser(new User(null,"UA120929","Homme","KHTOU","Otmane", new Date("20/12/2000"),"khtouotman@gmail.com","otman1234","0618566121", UserRole.STAGIAIRE,false));
+            userRepository.addUser(new User(null,"AB984256","Homme","EL IDRISSI","Zakaria", new Date("21/12/2000"),"elidrissizakaria@gmail.com","zaki1234","0612345678", UserRole.ADMIN,false));
+            userRepository.addUser(new User(null,"S452893","Homme","AKAJDAOU","Nabil", new Date("22/12/2000"),"akajdaounabil@gmail.com","nabil1234","0609876543",UserRole.EMPLOYER,false));
+            userRepository.addUser(new User(null,"S452893","Femme","Maali","Abir", new Date("23/12/2000"),"maaliabir@gmail.com","abir1234","0712896341",UserRole.STAGIAIRE,false));
+            userRepository.addUser(new User(null,"S452893","Homme","Boureqba","Ayoub", new Date("24/12/2000"),"ayoub@gmail.com","ayoub1234","0652147893",UserRole.STAGIAIRE_POTENTIEL,false));
             userRepository.getAllUsers().forEach(System.out::println);
 		};
 	}
